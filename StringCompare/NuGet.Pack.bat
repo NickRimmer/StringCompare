b18df6428d9e07550125917db7f781e0fbb864a2
Nick Rimmer <xan@dipteam.com>
2015-10-15 00:49:10 +0700

    NuGet Pack update

---
+++ StringCompare/NuGet.Pack.bat
@@ -0,0 +1,1 @@
+nuget pack StringCompare.csproj -Prop Configuration=Release
--- StringCompare/Properties/AssemblyInfo.cs
+++ StringCompare/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StringCompare")]
+[assembly: AssemblyTitle("Simple strings Compares (measuring the difference between two sequences)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nick Rimmer")]
